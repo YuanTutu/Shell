70eb22acc7a9e1d931417bdedd745e72cabf6a3d
yuanbo6 <38133429+YuanTutu@users.noreply.github.com>
2022-03-31 17:38:22 +0800

    Add files via upload

---
+++ 牛客网华为机试题目/入门/HJ46截取字符串.py
@@ -0,0 +1,6 @@
+word = str(input())
+k = int(input())
+val = []
+length=len(word)
+if 1<= k <= length:
+    print(word[:k])
---
+++ 牛客网华为机试题目/入门/HJ7取近似值.py
@@ -0,0 +1,5 @@
+a = float(input())
+b = round(a)
+if a-b==0.5:
+    b=b+1
+print(b)
